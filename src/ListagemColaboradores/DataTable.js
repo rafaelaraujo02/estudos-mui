c183163770e97b3472d5cfca6280b29109da7dfc
Rafael <rafaelaraujo.02@hotmail.com>
2022-11-23 15:39:55 -0300

    DataTable loading db persons

--- src/ListagemColaboradores/DataTable.js
+++ src/ListagemColaboradores/DataTable.js
@@ -19,12 +19,12 @@ export default function DataTable() {
         .then((resp => resp.json()))
         .then((data) => {
                             setPerson(data); 
-                            console.log(person)
+                            console.log((person[0].nome))
                         }
             )
         .catch(error => console.log(error))
       }, [])
-
+      
     const rows = [
         {   id: 1,
             nome: 'rafael araujo',
@@ -40,7 +40,7 @@ export default function DataTable() {
         { field: 'nome', headerName: 'Nome', width: 300 },
         { field: 'funcao', headerName: 'Função', width: 130 },
         { field: 'celular', headerName: 'Celular', width: 130 },
-        { field: 'email', headerName: 'E-mail', width: 180 },        
+        { field: 'email', headerName: 'E-mail', width: 220},        
         {
           field: 'acoes',
           headerName: 'Ações',
@@ -50,21 +50,24 @@ export default function DataTable() {
           ),
         },
       ];
+
+      function generateRandom() {
+        var length = 8,
+            charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789",
+            retVal = "";
+        for (var i = 0, n = charset.length; i < length; ++i) {
+            retVal += charset.charAt(Math.floor(Math.random() * n));
+        }
+        return retVal;
+    }
       
   return (
     <div style={{ height: 400, paddingLeft: '25%', width: '98%' }}>
-      {/* 
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection  
-      />
-      */}
+      
         <DataGrid
-             rows={rows}
+             rows={person}
              columns={columns}
+             getRowId={(row) =>  generateRandom()}
              pageSize={5}
              rowsPerPageOptions={[5]}
              checkboxSelection  
